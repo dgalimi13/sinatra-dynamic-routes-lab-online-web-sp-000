2914ea613dea32dc7c37e1d4c9d559205a9ca997
danny galimi <dannygalimi13@aim.com>
2021-03-05 03:41:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,30 @@
+require "pry"
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get "/reversename/:name" do
+    @user_name = params[:name]
+    @user_name.reverse
+  end
+  
+  get "/square/:number" do
+    @user_num = params[:number]
+   number = @user_num.to_i
+   square = number** 2
+   square_string = square.to_s
+    square_string
+  end 
 
+  get "/say/:number/:phrase" do 
+    @user_num = params[:number]
+    @user_phrase = params[:phrase]
+    number = @user_num.to_i
+    repeat = ""
+    number.times {repeat += @user_phrase}
+    return repeat
+    end 
+  
+  
+  
 end
